2107c157018592bb16f220edae5ba7c01460faee
Csant01 <csbhusal01@gmail.com>
2023-03-11 00:07:55 +1100

    {app name}v.0.2.2
    minor additions to create user

--- app/src/main/java/com/example/infs3605_app/CreateUserActivity.java
+++ app/src/main/java/com/example/infs3605_app/CreateUserActivity.java
@@ -3,13 +3,63 @@ package com.example.infs3605_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class CreateUserActivity extends AppCompatActivity {
+
+    TextView createUserName;
+    TextView createUserPass;
+    TextView createUserPassCheck;
+    TextView createUserEmail;
+    AutoCompleteTextView createUserType;
+    Button createUserButton;
+    String userName;
+    String userEmail;
+    String userPass;
+    String userPassCheck;
+    String userType;
+    ArrayList<String> userTypeArray = new ArrayList<>();
+    ArrayAdapter<String> userTypeAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+        userTypeArray.add("Student");
+        userTypeArray.add("UNSW Staff");
+        userTypeArray.add("Event Co-ordinator");
+        userTypeArray.add("UNSW Alumni");
+
+        createUserName = findViewById(R.id.createUserName);
+        createUserPass = findViewById(R.id.createUserPass);
+        createUserPassCheck = findViewById(R.id.createUserPassCheck);
+        createUserEmail = findViewById(R.id.createUserEmail);
+        createUserType = findViewById(R.id.createUserTypeSub);
+        createUserButton = findViewById(R.id.createUserButton);
+
+        userTypeAdapter = new ArrayAdapter<>(this, R.layout.user_type_list, userTypeArray);
+        createUserType.setAdapter(userTypeAdapter);
+
+        createUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userName = createUserName.getText().toString();
+                userPass = createUserPass.getText().toString();
+                userPassCheck = createUserPassCheck.getText().toString();
+                userEmail = createUserEmail.getText().toString();
+                userType = createUserType.getText().toString();
+            }
+        });
+
+
+
 
     }
 }
--- app/src/main/java/com/example/infs3605_app/LandingPageActivity.java
+++ app/src/main/java/com/example/infs3605_app/LandingPageActivity.java
@@ -2,13 +2,26 @@ package com.example.infs3605_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingPageActivity extends AppCompatActivity {
+
+    Button testBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+        testBtn = findViewById(R.id.testBtn);
+
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CreateUserActivity.class));
+            }
+        });
     }
 }
