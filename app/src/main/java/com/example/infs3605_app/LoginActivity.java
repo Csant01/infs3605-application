dcd3173655dcc704eb123d4538af8995494602ef
z5255052 <ariane.notaras@student.unsw.edu.au>
2023-03-18 22:16:52 +1100

    {UNSW Unite}v.03.3
    Login Page - minor updates to linking buttons and TextInputLayouts on LoginActivity.java

--- app/src/main/java/com/example/infs3605_app/LoginActivity.java
+++ app/src/main/java/com/example/infs3605_app/LoginActivity.java
@@ -1,14 +1,25 @@
 package com.example.infs3605_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class LoginActivity extends AppCompatActivity {
+
+    TextInputLayout emailInputLayout, passwordInputLayout;
+    AppCompatButton buttonLogin, buttonAlumniSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        buttonAlumniSignUp = findViewById(R.id.buttonAlumniSignUp);
+        emailInputLayout = findViewById(R.id.emailInputLayout);
+        passwordInputLayout = findViewById(R.id.passwordInputLayout);
+        buttonLogin = findViewById(R.id.buttonLogin);
     }
 }
