3bf8707d36fa565875a452ef434d7ae27bf45ebf
z5255052 <ariane.notaras@student.unsw.edu.au>
2023-03-18 23:25:12 +1100

    {UNSW Unite}v.03.4
    Create New Event - made .java  & .xml  for UNSW Staff to submit new events for approval.  Contains basic design for functional purposes and linked ID's in code

---
+++ app/src/main/java/com/example/infs3605_app/CreateEventActivity.java
@@ -0,0 +1,28 @@
+package com.example.infs3605_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.os.Bundle;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class CreateEventActivity extends AppCompatActivity {
+
+    TextInputLayout eventName, eventDate, eventTime, eventDescription;
+    AppCompatButton selectImage, submitEvent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+
+        eventName = findViewById(R.id.eventName);
+        eventDate = findViewById(R.id.eventDate);
+        eventTime = findViewById(R.id.eventTime);
+        eventDescription = findViewById(R.id.eventDescription);
+        selectImage = findViewById(R.id.selectImage);
+        submitEvent = findViewById(R.id.submitEvent);
+
+    }
+}
