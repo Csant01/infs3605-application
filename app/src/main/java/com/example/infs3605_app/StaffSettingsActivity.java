c25ea3d7b03e8710bf6ee927aa159c88884f6807
z5255052 <ariane.notaras@student.unsw.edu.au>
2023-04-01 15:18:56 +1100

    {UNSW Unite}v.6.2
    Created Settings pages (activities & .xmls) for students & staff

---
+++ app/src/main/java/com/example/infs3605_app/StaffSettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.infs3605_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StaffSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_settings);
+    }
+}
