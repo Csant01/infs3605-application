c5db7db8486336b5df36fa540bd0bc62dcd234b0
Fionaxliu <xfionaliu@gmail.com>
2023-04-09 18:14:38 +1000

    {UNSW Unite}v.0.11.1
    stuff

--- app/src/main/java/com/example/infs3605_app/ViewParticipantsActivity.java
+++ app/src/main/java/com/example/infs3605_app/ViewParticipantsActivity.java
@@ -2,6 +2,7 @@ package com.example.infs3605_app;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,18 +11,23 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class StudentFeedbackFormActivity extends AppCompatActivity {
+public class ViewParticipantsActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_feedback_form);
+        setContentView(R.layout.activity_view_participants);
 
-        // Bottom Navigation set for Past Events (student view)
+        // Set Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setTitle("Participants");
+        setSupportActionBar(toolbar);
+
+        // Bottom Navigation set for Events Page
         bottomNavigationView = findViewById(R.id.bottomNavigator);
-        bottomNavigationView.setSelectedItemId(R.id.pastEventsNavButton);
+        bottomNavigationView.setSelectedItemId(R.id.eventsNavButton);
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -29,35 +35,32 @@ public class StudentFeedbackFormActivity extends AppCompatActivity {
 
                 switch (item.getItemId())
                 {
-                    case R.id.homeNavButton:
-                        startActivity(new Intent(getApplicationContext(), StudentHomePageActivity.class));
+                    case R.id.dashboardNavButton:
+                        startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
                         overridePendingTransition(0,0);
                         return true;
 
-                    case R.id.savedNavButton:
-                        startActivity(new Intent(getApplicationContext(), StudentSavedEventsActivity.class));
+                    case R.id.addEventNavButton:
+                        startActivity(new Intent(getApplicationContext(), CreateEventActivity.class));
                         overridePendingTransition(0,0);
                         return true;
 
-                    case R.id.allEventsNavButton:
-                        startActivity(new Intent(getApplicationContext(), StudentAllEventsActivity.class));
+                    case R.id.approvalsNavButton:
+                        startActivity(new Intent(getApplicationContext(), EventsPendingApprovalActivity.class));
                         overridePendingTransition(0,0);
                         return true;
 
-                    case R.id.followingNavButton:
-                        startActivity(new Intent(getApplicationContext(), StudentFollowingListActivity.class));
+                    case R.id.feedbackNavButton:
+                        startActivity(new Intent(getApplicationContext(), EventFeedbackActivity.class));
                         overridePendingTransition(0,0);
                         return true;
 
-                    case R.id.pastEventsNavButton:
+                    case R.id.eventsNavButton:
                         return true;
                 }
                 return false;
             }
         });
-    }
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
+    }
 }
