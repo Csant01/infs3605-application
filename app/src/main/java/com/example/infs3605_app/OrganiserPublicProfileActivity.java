8a0ad3aae24cedc875c7216d633ffc2f1a22f755
Fionaxliu <xfionaliu@gmail.com>
2023-04-16 01:03:58 +1000

    {UNSW Unite}v.0.13.5
    fixes made to event detail (student side and admin side), toolbar added to following page (student side), swipe card recyclerview sizing fixed (REQUIRES CHECKING ON EMULATOR), and organiser public profile bottom navigation added and coded.

--- app/src/main/java/com/example/infs3605_app/OrganiserPublicProfileActivity.java
+++ app/src/main/java/com/example/infs3605_app/OrganiserPublicProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.infs3605_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -8,12 +9,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -40,10 +45,49 @@ public class OrganiserPublicProfileActivity extends AppCompatActivity implements
     boolean bool;
     private static final String TAG = "OrganiserPublicProfile";
 
+    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organiser_public_profile);
+
+        // Bottom Navigation set for Organiser Public Profile
+        bottomNavigationView = findViewById(R.id.bottomNavigator);
+        bottomNavigationView.setSelectedItemId(R.id.homeNavButton);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId())
+                {
+                    case R.id.savedNavButton:
+                        startActivity(new Intent(getApplicationContext(), StudentSavedEventsActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.allEventsNavButton:
+                        startActivity(new Intent(getApplicationContext(), StudentAllEventsActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.pastEventsNavButton:
+                        startActivity(new Intent(getApplicationContext(), StudentPastEventsActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.followingNavButton:
+                        startActivity(new Intent(getApplicationContext(), StudentFollowingListActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.homeNavButton:
+                        return true;
+                }
+                return false;
+            }
+        });
 
         String loggedIn = User.currentlyLoggedIn.get(User.currentlyLoggedIn.size()-1);
         organiserName = findViewById(R.id.organiserNamePrint);
